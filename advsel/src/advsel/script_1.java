6102e88c854eaee7f7c24b1b8521fe76846814c4
Gajanand <Gajanand@Gajaa>
2022-12-27 15:25:16 +0530

    script_1

---
+++ advsel/src/advsel/script_1.java
@@ -0,0 +1,15 @@
+package advsel;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class script_1 
+{
+	public static void main(String[] args) 
+	{
+		String key="webdriver.chrome.driver";
+		String value="./soft/chromedriver.exe";
+		System.setProperty(key, value);
+		 ChromeDriver c = new ChromeDriver();
+	}
+
+}
